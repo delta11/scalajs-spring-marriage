556d8ebac26a80301a944912746e7ea3826d43dd
Thomas van Putten <thomasvanputten@hotmail.com>
2017-06-07 21:25:16 +0200

    cleaned up scala a bit

--- scalajs/src/main/scala/com/example/TutorialApp.scala
+++ scalajs/src/main/scala/com/example/TutorialApp.scala
@@ -4,17 +4,7 @@ import scala.scalajs.js
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExportTopLevel
 
-object TutorialApp extends JSApp {
-  def main(): Unit = {
-    println("Hello world!")
-    println("Hello asdf!")
-    println("Hello sdf!")
-    println("Hello worlzxcfvd!")
-    if(1>2){
-      println("Hello sdf!")
-    }
-  }
-
+object TutorialApp {
   var count = 0
   @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(callback: js.Dynamic): Unit = {
